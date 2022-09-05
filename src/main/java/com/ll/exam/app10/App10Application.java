c50ce67738e3020bde97e3a039dcaf272c28221d
jdi022222 <97517890+jdi022222@users.noreply.github.com>
2022-09-05 15:02:25 +0900

    추가 세팅

--- src/main/java/com/ll/exam/app10/App10Application.java
+++ src/main/java/com/ll/exam/app10/App10Application.java
@@ -2,8 +2,10 @@ package com.ll.exam.app10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 생성일자, 수정일자 자동으로 등록
 public class App10Application {
 
 	public static void main(String[] args) {
